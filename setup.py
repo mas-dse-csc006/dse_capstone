269436602c9513911c2d8e3af09195235b75e9e1
CHRIS CHEN <utcsox@gmail.com>
2018-06-05 21:40:37 -0700

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Amazon Data Analysis',
+    author='cohort3group1',
+    license='',
+)
